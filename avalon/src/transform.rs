a6b6ead32701cfdf4e36c71834b4820ea80d8e60
Bailey Danyluk <baileydanyluk@gmail.com>
2025-01-05 19:13:40 -0700

    add transform class

---
+++ avalon/src/transform.rs
@@ -0,0 +1,173 @@
+use nalgebra_glm::{ quat, Quat, quat_to_mat4, vec3, Vec3, vec4, Mat4 };
+use nalgebra::Unit;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Euler {
+    axis_x: f32,
+    axis_y: f32,
+    axis_z: f32,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Transform {
+    position: Vec3,
+    orientation_quaternion: Quat,
+    transformation_matrix: Mat4,
+    dirty: bool
+}
+
+impl Transform {
+    pub fn new() -> Transform {
+        Transform {
+            position: Vec3::zeros(),
+            orientation_quaternion: Quat::default(),
+            transformation_matrix: Mat4::identity(),
+            dirty: true
+        }
+    }
+
+    pub fn matrix(&self) -> Mat4 {
+        if self.dirty {
+            quat_to_mat4(&self.orientation_quaternion) * Mat4::from_columns(&[
+                vec4(1.0, 0.0, 0.0, 0.0),
+                vec4(0.0, 1.0, 0.0, 0.0),
+                vec4(0.0, 0.0, 1.0, 0.0),
+                vec4(self.position.x, self.position.y, self.position.z, 1.0)
+            ])
+        } else {
+            self.transformation_matrix
+        }
+    }
+
+    pub fn clean(&mut self) {
+        if self.dirty {
+            self.transformation_matrix = self.matrix();
+            self.dirty = false;
+        }
+    }
+
+    pub fn set_euler_angles(&mut self, euler: Euler) {
+        self.dirty = true;
+        self.orientation_quaternion =
+            Quat::from_polar_decomposition(1.0, euler.axis_x, Unit::new_normalize(vec3(1.0, 0.0, 0.0))) *
+            Quat::from_polar_decomposition(1.0, euler.axis_y, Unit::new_normalize(vec3(0.0, 1.0, 0.0))) *
+            Quat::from_polar_decomposition(1.0, euler.axis_z, Unit::new_normalize(vec3(0.0, 0.0, 1.0)));
+    }
+
+    pub fn euler_angles(&self) -> Euler {
+        self.matrix().into()
+    }
+
+    pub fn set_position(&mut self, position: Vec3) {
+        self.dirty = true;
+        self.position = position;
+    }
+
+    pub fn position(&self) -> Vec3 {
+        self.position
+    }
+
+    pub fn translate(&mut self, offset: Vec3) {
+        self.set_position(self.position() + offset);
+    }
+}
+
+impl From<&Transform> for Euler {
+    fn from(transform: &Transform) -> Euler {
+        transform.euler_angles()
+    }
+}
+
+impl std::ops::Mul for Transform {
+    type Output = Transform;
+    fn mul(self, rhs: Transform) -> Transform {
+        let mut transform = Transform {
+            position: self.position + rhs.position,
+            orientation_quaternion: self.orientation_quaternion * rhs.orientation_quaternion,
+            transformation_matrix: Mat4::identity(),
+            dirty: true
+        };
+        transform.clean();
+        transform
+    }
+}
+
+impl Default for Euler {
+    fn default() -> Euler {
+        Euler {
+            axis_x: 0.0,
+            axis_y: 0.0,
+            axis_z: 0.0
+        }
+    }
+}
+
+impl From<Mat4> for Euler {
+    fn from(transform: Mat4) -> Euler {
+        let (theta, psi, phi) = if transform.m31.abs() != 1.0 {
+            let theta = -transform.m32.asin();
+            let cos_theta = theta.cos();
+            let psi = (transform.m32 / cos_theta).atan2(transform.m33 / cos_theta);
+            let phi = (transform.m21 / cos_theta).atan2(transform.m11 / cos_theta);
+
+            (theta, psi, phi)
+        } else {
+            let phi = 0.0;
+            let (theta, psi) = if transform.m31 == -1.0 {
+                let theta = std::f32::consts::FRAC_PI_2;
+                let psi = phi + transform.m12.atan2(transform.m13);
+                (theta, psi)
+            } else {
+                let theta = -std::f32::consts::FRAC_PI_2;
+                let psi = -phi + (-transform.m12).atan2(-transform.m13);
+                (theta, psi)
+            };
+
+            (theta, psi, phi)
+        };
+
+        Euler {
+            axis_x: psi,
+            axis_y: theta,
+            axis_z: phi
+        }
+    }
+}
+
+impl From<Euler> for Mat4 {
+    fn from(euler: Euler) -> Mat4 {
+        let cos_theta = euler.axis_y.cos();
+        let sin_theta = euler.axis_y.sin();
+        let cos_psi = euler.axis_x.cos();
+        let sin_psi = euler.axis_x.sin();
+        let cos_phi = euler.axis_z.cos();
+        let sin_phi = euler.axis_z.sin();
+
+        let r1 = vec4(
+            cos_theta * cos_phi,
+            sin_psi * sin_theta * sin_phi - cos_psi * sin_phi,
+            cos_psi * sin_theta * cos_phi + sin_psi * sin_phi,
+            0.0
+        ).transpose();
+        let r2 = vec4(
+            cos_theta * sin_phi,
+            sin_psi * sin_theta * sin_phi + cos_psi * cos_phi,
+            cos_psi * sin_theta * sin_phi - sin_psi * cos_phi,
+            0.0
+        ).transpose();
+        let r3 = vec4(
+            -sin_theta,
+            sin_psi * cos_theta,
+            cos_psi * cos_theta,
+            0.0
+        ).transpose();
+        let r4 = vec4(
+            0.0,
+            0.0,
+            0.0,
+            1.0
+        ).transpose();
+
+        Mat4::from_rows(&[r1, r2, r3, r4])
+    }
+}
--- game/src/main.rs
+++ game/src/main.rs
@@ -4,15 +4,6 @@ use nalgebra_glm::vec2;
 fn main() {
     let mut engine = avalon::engine();
 
-    let framebuffer = avalon::viewport::Viewport::new(vec2(1920, 1080))
-        .colour_attachment().format(avalon::texture::gpu::SizedComponent::RGBA8)
-        .colour_attachment().format(avalon::texture::gpu::SizedComponent::R8)
-        .colour_attachment().format(avalon::texture::gpu::SizedComponent::FloatRGBA32)
-        //.depth_stencil(avalon::viewport::DepthStencil::Depth)
-        .build();
-
-    framebuffer.bind().clear();
-
     while engine.is_open() {
         engine.start_frame();
         engine.poll_events();
