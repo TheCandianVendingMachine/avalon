96d4620cf9cdbc6d5a7bfaf4eab3655610e8acc2
Bailey Danyluk <baileydanyluk@gmail.com>
2025-01-20 15:34:56 -0700

    add helper function to get asset from bundle

--- avalon_asset/src/bundle.rs
+++ avalon_asset/src/bundle.rs
@@ -5,3 +5,22 @@ pub struct Bundle {
     pub group: Vec<asset::Asset>,
     pub name: String,
 }
+
+impl Bundle {
+    pub fn asset(&self, tag: impl Into<String>) -> Option<&asset::Asset> {
+        let tag = tag.into();
+        for asset in self.group.iter() {
+            if asset.metadata.tag == tag {
+                return Some(asset);
+            }
+        }
+        return None;
+    }
+}
+
+impl std::ops::Index<String> for Bundle {
+    type Output = asset::Asset;
+    fn index(&self, index: String) -> &asset::Asset {
+        self.asset(index).expect("Tag needs to exist within bundle!")
+    }
+}
