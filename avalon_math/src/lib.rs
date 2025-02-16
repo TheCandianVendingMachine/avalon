20c6ea3cd658cd11492ae1eb9aa34a76422cd82c
Bailey Danyluk <baileydanyluk@gmail.com>
2025-02-16 03:25:39 -0700

    Add SSE2 support for vector addition

--- avalon_math/src/lib.rs
+++ avalon_math/src/lib.rs
@@ -1,5 +1,71 @@
+pub mod simd {
+    #[allow(non_camel_case_types)]
+    #[repr(C)]
+    #[repr(align(16))]
+    pub struct f32x4(pub f32, pub f32, pub f32, pub f32);
+
+    #[allow(non_camel_case_types)]
+    pub enum Type {
+        u8,
+        u16,
+        u32,
+        u64,
+        i8,
+        i16,
+        i32,
+        i64,
+        f32,
+        f64
+    }
+
+    impl Type {
+        pub fn convert_variable<T: SimdType, U: SimdType + Copy>(var: T) -> U {
+            let var_ptr = &var as *const T;
+            let var_ptr = var_ptr as *const std::ffi::c_void;
+            match (T::to_type(), U::to_type()) {
+                (Type::f64, Type::f64) |
+                (Type::f32, Type::f32) |
+                (Type::i64, Type::i64) |
+                (Type::i32, Type::i32) |
+                (Type::i16, Type::i16) |
+                (Type::i8,  Type::i8)  |
+                (Type::u64, Type::u64) |
+                (Type::u32, Type::u32) |
+                (Type::u16, Type::u16) |
+                (Type::u8, Type::u8) => unsafe {
+                    let dst_ptr: *const U = std::mem::transmute(var_ptr);
+                    *dst_ptr
+                },
+                _ => {
+                    panic!("Attempting to convert a variable which does not have same type");
+                }
+            }
+        }
+    }
+
+    pub trait SimdType {
+        fn to_type() -> Type;
+    }
+
+    impl SimdType for i8 { fn to_type() -> Type { Type::i8 } }
+    impl SimdType for i16 { fn to_type() -> Type { Type::i16 } }
+    impl SimdType for i32 { fn to_type() -> Type { Type::i32 } }
+    impl SimdType for i64 { fn to_type() -> Type { Type::i64 } }
+    impl SimdType for u8 { fn to_type() -> Type { Type::u8 } }
+    impl SimdType for u16 { fn to_type() -> Type { Type::u16 } }
+    impl SimdType for u32 { fn to_type() -> Type { Type::u32 } }
+    impl SimdType for u64 { fn to_type() -> Type { Type::u64 } }
+    impl SimdType for f32 { fn to_type() -> Type { Type::f32 } }
+    impl SimdType for f64 { fn to_type() -> Type { Type::f64 } }
+}
 
 pub mod scalar;
+#[cfg(all(
+    target_arch = "x86_64",
+    target_feature = "sse2"
+    )
+)]
+
 pub mod sse2;
 pub mod matrix1;
 pub mod matrix2;
@@ -10,20 +76,20 @@ pub mod vector2;
 pub mod vector3;
 pub mod vector4;
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Vector1<TElem> {
     pub x: TElem
 }
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Vector2<TElem> {
     pub x: TElem,
     pub y: TElem,
 }
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Vector3<TElem> {
     pub x: TElem,
@@ -31,7 +97,7 @@ pub struct Vector3<TElem> {
     pub z: TElem,
 }
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Vector4<TElem> {
     pub x: TElem,
@@ -40,13 +106,13 @@ pub struct Vector4<TElem> {
     pub w: TElem,
 }
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Matrix1<TElem> {
     pub m11: TElem
 }
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Matrix2<TElem> {
     pub m11: TElem,
@@ -55,7 +121,7 @@ pub struct Matrix2<TElem> {
     pub m22: TElem,
 }
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Matrix3<TElem> {
     pub m11: TElem,
@@ -69,7 +135,7 @@ pub struct Matrix3<TElem> {
     pub m33: TElem,
 }
 
-#[repr(align(8))]
+#[repr(align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct Matrix4<TElem> {
     pub m11: TElem,
---
+++ avalon_math/src/sse2/vector4.rs
@@ -0,0 +1,268 @@
+use std::ops::{ Add, Sub, Mul, Div, Neg };
+use crate::scalar::vector4 as scalar;
+use crate::simd::{ self, SimdType };
+use crate::Vector4;
+use crate::scalar::HasSqrt;
+use std::arch::x86_64 as simd_inst;
+
+pub fn add<U, T>(lhs: Vector4<T>, rhs: Vector4<T>) -> Vector4<U> where
+    T: SimdType + Copy + Add<Output = U>,
+    U: SimdType + Copy {
+    match T::to_type() {
+        simd::Type::f32 => {
+            let lhs_pack = simd::f32x4(
+                simd::Type::convert_variable(lhs.x),
+                simd::Type::convert_variable(lhs.y),
+                simd::Type::convert_variable(lhs.z),
+                simd::Type::convert_variable(lhs.w)
+            );
+
+            let rhs_pack = simd::f32x4(
+                simd::Type::convert_variable(rhs.x),
+                simd::Type::convert_variable(rhs.y),
+                simd::Type::convert_variable(rhs.z),
+                simd::Type::convert_variable(rhs.w)
+            );
+
+            unsafe {
+                let simd_lhs = simd_inst::_mm_load_ps(&lhs_pack.0);
+                let simd_rhs = simd_inst::_mm_load_ps(&rhs_pack.0);
+                let simd_result = simd_inst::_mm_add_ps(simd_lhs, simd_rhs);
+
+                let mut result = simd::f32x4(0.0, 0.0, 0.0, 0.0);
+                simd_inst::_mm_store_ps(&mut result.0, simd_result);
+
+                Vector4 {
+                    x: simd::Type::convert_variable(result.0),
+                    y: simd::Type::convert_variable(result.1),
+                    z: simd::Type::convert_variable(result.2),
+                    w: simd::Type::convert_variable(result.3),
+                }
+            }
+        },
+        _ => {
+            scalar::add(lhs, rhs)
+        }
+    }
+}
+
+pub fn sub<U, T>(lhs: Vector4<T>, rhs: Vector4<T>) -> Vector4<U> where
+    T : Copy + Sub<Output = U> {
+    scalar::sub(lhs, rhs)
+}
+
+pub fn mul<U, T>(lhs: Vector4<T>, rhs: T) -> Vector4<U> where
+    T : Copy + Mul<Output = U> {
+    scalar::mul(lhs, rhs)
+}
+
+pub fn div_with_numerator<U, T>(lhs: T, rhs: Vector4<T>) -> Vector4<U> where
+    T : Copy + Div<Output = U> {
+    scalar::div_with_numerator(lhs, rhs)
+}
+
+pub fn div_with_denominator<U, T>(lhs: Vector4<T>, rhs: T) -> Vector4<U> where
+    T : Copy + Div<Output = U> {
+    scalar::div_with_denominator(lhs, rhs)
+}
+
+pub fn component_mul<U, T>(lhs: Vector4<T>, rhs: Vector4<T>) -> Vector4<U> where
+    T : Copy + Mul<Output = U> {
+    scalar::component_mul(lhs, rhs)
+}
+
+pub fn dot<U, T>(lhs: Vector4<T>, rhs: Vector4<T>) -> U where
+    T : Copy + Mul<Output = U>,
+    U: Add<Output = U> {
+    scalar::dot(lhs, rhs)
+}
+
+pub fn magnitude<U, T>(vec: Vector4<T>) -> U where
+    T : Copy + Mul<Output = U>,
+    U: Add<Output = U> + HasSqrt {
+    scalar::magnitude(vec)
+}
+
+pub fn magnitude_sqr<U, T>(vec: Vector4<T>) -> U where
+    T : Copy + Mul<Output = U>,
+    U: Add<Output = U> {
+    scalar::magnitude_sqr(vec)
+}
+
+pub fn negate<U, T>(vec: Vector4<T>) -> Vector4<U> where
+    T: Neg<Output = U> {
+    scalar::negate(vec)
+}
+
+#[cfg(test)]
+mod test_f32 {
+    use approx::assert_abs_diff_eq;
+    use crate::Vector4;
+    use crate::sse2::vector4;
+
+    #[test]
+    fn addition() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let b = Vector4 {
+            x: 3.0_f32,
+            y: 9.0_f32,
+            z: 0.8_f32,
+            w: 7.0_f32
+        };
+        let c = vector4::add(a, b);
+        assert_abs_diff_eq!(c.x, 8.0_f32);
+        assert_abs_diff_eq!(c.y, 21.0_f32);
+        assert_abs_diff_eq!(c.z, -2.7_f32);
+        assert_abs_diff_eq!(c.w, 9.0_f32);
+    }
+
+    #[test]
+    fn sub() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let b = Vector4 {
+            x: 3.0_f32,
+            y: 9.0_f32,
+            z: 0.8_f32,
+            w: 7.0_f32
+        };
+        let c = vector4::sub(b, a);
+        assert_abs_diff_eq!(c.x, -2.0_f32);
+        assert_abs_diff_eq!(c.y, -3.0_f32);
+        assert_abs_diff_eq!(c.z, 4.3_f32);
+        assert_abs_diff_eq!(c.w, 5.0_f32);
+    }
+
+    #[test]
+    fn mul() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: 0.8_f32,
+            w: 2.0_f32
+        };
+        let b = 3.0_f32;
+        let c = vector4::mul(a, b);
+        assert_abs_diff_eq!(c.x, 15.0_f32);
+        assert_abs_diff_eq!(c.y, 36.0_f32);
+        assert_abs_diff_eq!(c.z, 2.4000000000000004_f32);
+        assert_abs_diff_eq!(c.w, 6.0_f32);
+    }
+
+    #[test]
+    fn div_with_numerator() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let c = vector4::div_with_numerator(10.0_f32, a);
+        assert_abs_diff_eq!(c.x, 2.0_f32);
+        assert_abs_diff_eq!(c.y, 0.8333333333333334_f32);
+        assert_abs_diff_eq!(c.z, -2.857142857142857_f32);
+        assert_abs_diff_eq!(c.w, 5.0_f32);
+    }
+
+    #[test]
+    fn div_with_denominator() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let c = vector4::div_with_denominator(a, 10.0_f32);
+        assert_abs_diff_eq!(c.x, 0.5_f32);
+        assert_abs_diff_eq!(c.y, 1.2_f32);
+        assert_abs_diff_eq!(c.z, -0.35_f32);
+        assert_abs_diff_eq!(c.w, 0.2_f32);
+    }
+
+    #[test]
+    fn component_mul() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let b = Vector4 {
+            x: 6.7_f32,
+            y: 8.7_f32,
+            z: 5.0_f32,
+            w: 3.4_f32,
+        };
+        let c = vector4::component_mul(a, b);
+        assert_abs_diff_eq!(c.x, 33.5_f32);
+        assert_abs_diff_eq!(c.y, 104.399994_f32);
+        assert_abs_diff_eq!(c.z, -17.5_f32);
+        assert_abs_diff_eq!(c.w, 6.8_f32);
+    }
+
+    #[test]
+    fn dot() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let b = Vector4 {
+            x: 6.7_f32,
+            y: 8.7_f32,
+            z: 5.0_f32,
+            w: 3.4_f32
+        };
+        let c = vector4::dot(a, b);
+        assert_abs_diff_eq!(c, 127.19999999999997_f32);
+    }
+
+    #[test]
+    fn magnitude() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let c = vector4::magnitude(a);
+        assert_abs_diff_eq!(c, 13.6106575888162_f32);
+    }
+
+    #[test]
+    fn magnitude_sqr() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let c = vector4::magnitude_sqr(a);
+        assert_abs_diff_eq!(c, 185.25_f32);
+    }
+
+    #[test]
+    fn neg() {
+        let a = Vector4 {
+            x: 5.0_f32,
+            y: 12.0_f32,
+            z: -3.5_f32,
+            w: 2.0_f32
+        };
+        let c = vector4::negate(a);
+        assert_abs_diff_eq!(c.x, -5.0_f32);
+        assert_abs_diff_eq!(c.y, -12.0_f32);
+        assert_abs_diff_eq!(c.z, 3.5_f32);
+        assert_abs_diff_eq!(c.w, -2.0_f32);
+    }
+}
